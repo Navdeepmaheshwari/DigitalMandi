f47394d216c6756a497fc207edc8ffbc94238c4d
Amish Kumar Singh <amish.kumar1211@gmail.com>
2022-03-29 18:54:39 +0530

    make App function body empty in app.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import './App.css';
 // import { SignUpForm } from './Components/farmer forms/FSignUpForm';
 // import { LoginForm } from './Components/farmer forms/FLoginForm';
 // import {Navbar} from './Components/nav links/CNav';
-import {Hero} from './Components/hero-section/CHero';
+// import {Hero} from './Components/hero-section/CHero';
 function App() {
   return (
     <div className="App">
-      <Hero/>   
+      
     </div>
   );
 }
