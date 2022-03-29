a000f933df939eaa364ce6ef21b2aa20c3f69f48
Amish Kumar Singh <amish.kumar1211@gmail.com>
2022-03-29 13:37:20 +0530

    Add CNav in Component  folder

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { SignUpForm } from './Components/farmer forms/FSignUpForm';
 import { LoginForm } from './Components/farmer forms/FLoginForm';
-
+import {Navbar} from './Components/nav links/CNav';
 function App() {
   return (
     <div className="App">
       {/* <SignUpForm/> */}
       {/* <LoginForm/> */}
+      {/* <Navbar/> */}
+    
     </div>
   );
 }
