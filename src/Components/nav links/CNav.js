72c7b10d99897fb0df938ecb660ad57f6f97860c
NAVDEEP MAHESHWARI <navdeepmaheshwari2002@gmail.com>
2022-03-29 15:19:42 +0530

    login signup routing done

--- src/Components/nav links/CNav.js
+++ src/Components/nav links/CNav.js
@@ -1,20 +1,20 @@
 import React from 'react'
 import './CNav.css';
-
+import { Link } from "react-router-dom";
 export const Navbar = () => {
     return (
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container">
-        <a class="navbar-brand" href="#">D-Mandi</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+        <a className="navbar-brand" href="#">D-Mandi</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse d-flex flex-row-reverse " id="navbarNavAltMarkup">
-          <div class="navbar-nav">
-            <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-            <a class="nav-item nav-link" href="#">About Us</a>
-            <a class="nav-item nav-link" href="#">Sign in</a>
-            <a class="nav-item nav-link" href="#">Sign up</a>
+        <div className="collapse navbar-collapse d-flex flex-row-reverse " id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            <a className="nav-item nav-link active" href="/">Home</a>
+            <a className="nav-item nav-link" href="/about">About Us</a>
+            <a className="nav-item nav-link" href="/login">Sign in</a>
+            <a className="nav-item nav-link" href="/signup">Sign up</a>
           </div>
         </div>
         </div>
