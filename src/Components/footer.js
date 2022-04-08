baf1f60b6f1e52d222cf18a9f2eb7b8bfe1db822
jimpatel247 <pateljim2002@gmail.com>
2022-04-09 00:07:08 +0530

    footer setting

--- src/Components/footer.js
+++ src/Components/footer.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export const Footer = () => {
   return (
     <>
-      <footer className="bg-dark text-secondary text-center text-lg-start mt-4">
+      <footer className="bg-dark text-secondary text-center text-lg-start mt-4 fixed-bottom ">
         {/* <!-- Copyright --> */}
         <div className="text-center p-3 ">
           © 2022 Copyright :
